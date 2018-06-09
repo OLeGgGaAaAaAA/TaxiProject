8efa58a8f2c299f8508d17ba0e16b5e9d0c23189
OLeGgGaAaAaAA <olega1297>
2018-06-09 22:20:28 +0300

    future front

--- src/main/resources/static/angular/app.js/driverAuth.js
+++ src/main/resources/static/angular/app.js/driverAuth.js
@@ -1,3 +1,20 @@
 function driverAuth() {
-    
+    var doc = document;
+    var login = doc.getElementsByName("login");
+    var password = doc.getElementsByName("password");
+    $.ajax({
+        type: "POST",
+        url: "/TaxiProject",
+        dataType: "json",
+        data: {requestType: "driverAuth", login: login, password: password},
+        success: function (data) {
+            if(data.result.equals("login_exists")){
+                alert("Такой логин уже существует")
+            }else if(data.result.equals("complete")) {
+                location.href = "driverPage.html"
+            }else if(data.result.equals("error")) {
+                alert("Ошибка")
+            }   location.reload(true)
+        }
+    });
 }
